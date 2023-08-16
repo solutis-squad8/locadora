593ba8fd66c58822349cd8f1db7452d93d4bef6b
Cainan Lima <cainan.lima25@gmail.com>
2023-08-16 17:18:52 -0300

    cria o mapping do tipo get para motorista

--- src/main/java/br/com/solutis/locadora/controller/BaseCrudController.java
+++ src/main/java/br/com/solutis/locadora/controller/BaseCrudController.java
@@ -4,15 +4,23 @@ package br.com.solutis.locadora.controller;
 import br.com.solutis.locadora.service.BaseCrudService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 public abstract class BaseCrudController<T> {
     private BaseCrudService service;
 
     public BaseCrudController(BaseCrudService service){
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<T>> obterTodos(){
+       return ResponseEntity.ok( (List<T>) service.obterTodos());
     }
 
     @PostMapping
