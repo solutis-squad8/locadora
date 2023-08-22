ef95bf43b534a4d93dcfbda8726fa38a5153864d
Victor <victordosanjos19@gmail.com>
2023-08-22 00:24:28 -0300

    Adicionando Swagger

---
+++ src/main/java/br/com/solutis/locadora/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package br.com.solutis.locadora.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Locadora de Carros - Squad 8",
+                description = "Desafio 02 #Locadora de Carros do curso de nivelamento da Solutis"
+
+        )
+)
+public class SwaggerConfig {
+}
--- src/main/java/br/com/solutis/locadora/controller/AluguelController.java
+++ src/main/java/br/com/solutis/locadora/controller/AluguelController.java
@@ -2,6 +2,8 @@ package br.com.solutis.locadora.controller;
 
 import br.com.solutis.locadora.model.form.AluguelInsertForm;
 import br.com.solutis.locadora.service.AluguelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Nullable;
 import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Positive;
@@ -14,10 +16,15 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/aluguel")
+@Tag(name="Aluguel Controller")
 public class AluguelController {
 
     private final AluguelService aluguelService;
 
+
+    @Operation(
+            summary =  "Adiciona um aluguel"
+    )
     @PostMapping
     @Transactional
     public ResponseEntity<?> addAluguel(@RequestBody AluguelInsertForm aluguelInsertForm){
@@ -25,11 +32,17 @@ public class AluguelController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(
+            summary =  "Retorna os aluguéis de um motorista"
+    )
     @GetMapping("/motorista/{id}")
     public ResponseEntity<?> obterAlugueisPorMotoristaId(@Nullable @Positive @PathVariable Long id){;
         return ResponseEntity.ok().body(aluguelService.obterAluguelPorMotoristaId(id));
     }
 
+    @Operation(
+            summary =  "Retorna todos os aluguéis cadastrados"
+    )
     @GetMapping
     public ResponseEntity<?> obterTodos() {
         return ResponseEntity.ok().body(aluguelService.obterTodos());
